27e11926d98662914b0b91fda0f4f51e3ce71db4
Sidhant <blackjack.pandey00@gmail.com>
2021-10-03 18:19:14 +0530

    Update CredentialsForMail.js

--- Backend/rotues/auth/CredentialsForMail.js
+++ Backend/rotues/auth/CredentialsForMail.js
@@ -3,8 +3,8 @@ const nodemailer = require("nodemailer");
 var transporter = nodemailer.createTransport({
   service: "gmail",
   auth: {
-    user: "sidhant.seraphic@gmail.com",
-    pass: "Seraphic1999",
+    user: "",
+    pass: "",
   },
 });
 
